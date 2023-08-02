0eefc47d30c4550f9013bc5330513b2f82da8596
Sunil Shetye <sunilshetye@rocketmail.com>
2023-08-03 00:32:01 +0530

    making code async

--- app1/consumers.py
+++ app1/consumers.py
@@ -1,17 +1,49 @@
 import json
 
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class AnnouncementConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+class AnnouncementConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user_name = self.scope['url_route']['kwargs']['user_name']
+        self.user_group_name = f'announcement_{self.user_name}'
+        print(f'connect: group_name={self.user_group_name}')
 
-    def disconnect(self, close_code):
-        pass
+        # Join user group
+        await self.channel_layer.group_add(self.user_group_name, self.channel_name)
 
-    def receive(self, text_data):
+        await self.accept()
+
+        # Send message to user group
+        message = 'Connected'
+        send_message = {
+            'type': 'announcement.message',
+            'message': message
+        }
+        await self.channel_layer.group_send(self.user_group_name, send_message)
+
+    async def disconnect(self, close_code):
+        print(f'disconnect: group_name={self.user_group_name}')
+        # Leave user group
+        await self.channel_layer.group_discard(self.user_group_name, self.channel_name)
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        print(f'receive: group_name={self.user_group_name}')
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
 
+        # Send message to user group
+        message = text_data_json['message']
+        send_message = {
+            'type': 'announcement.message',
+            'message': message
+        }
+        await self.channel_layer.group_send(self.user_group_name, send_message)
+
+    # Receive message from user group
+    async def announcement_message(self, event):
+        print(f'announcement_message: group_name={self.user_group_name}')
+        message = event['message']
+
+        # Send message to WebSocket
         self.send(text_data=json.dumps({'message': message}))
